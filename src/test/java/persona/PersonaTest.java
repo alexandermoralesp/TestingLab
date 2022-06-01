f056ab5dfe2641c87aca9ab034e1c3217dd3be94
paulozterra <66433825+paulozterra@users.noreply.github.com>
2022-06-01 00:48:24 -0500

    test

--- src/test/java/persona/PersonaTest.java
+++ src/test/java/persona/PersonaTest.java
@@ -1,17 +1,14 @@
 package persona;
+
 import junit.framework.TestCase;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Locale;
-import java.util.zip.DataFormatException;
 
 public class PersonaTest extends TestCase {
     Persona persona;
@@ -30,4 +27,45 @@ public class PersonaTest extends TestCase {
     public void testMayorDeEdad() throws ParseException {
         assertFalse("Es menor de edad", persona.esMayorDeEdad());
     }
+    @Test
+    public void testgetname() throws ParseException{
+         assertEquals("Alexander",persona.getName());
+    }
+    @Test
+    public void testgetlastname() throws ParseException {
+        assertEquals("Morales Panitz",persona.getLastName());
+    }
+    @Test
+    public void testgetnacimiento() throws ParseException{
+        assertNotNull(persona.getNacimiento());
+    }
+    @Test
+    public void testgetconsulta() throws ParseException{
+        assertNotNull(persona.getConsulta());
+    }
+
+    @Test
+    public void testsetname()throws ParseException {
+        persona.setName("paulo");
+        assertEquals(persona.getName(),"paulo");
+    }
+
+    @Test
+    public void testsetlastname()throws ParseException{
+        persona.setLastName("Cuaresma Puclla");
+        assertEquals(persona.getLastName(),"Cuaresma Puclla");
+    }
+
+    @Test
+    public void testsetconsulta()throws ParseException{
+        persona.setConsulta(LocalDate.parse("03/01/2018", sdf));
+        assertEquals(persona.getConsulta(),LocalDate.parse("03/01/2018", sdf));
+    }
+
+    @Test
+    public void testsetnacimiento()throws ParseException{
+        persona.setNacimiento(LocalDate.parse("02/29/2000", sdf));
+        assertEquals(persona.getNacimiento(),LocalDate.parse("02/29/2000", sdf));
+    }
 }
+
